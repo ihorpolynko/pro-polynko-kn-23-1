b2874d29d49e42bd7e53d5250ee58ecec3610ed4
Ігор Полинько <igor.polynko@gmail.com>
2025-05-07 14:24:09 +0300

    Added LB6

---
+++ LB6/1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace LB6PROPolynkoKN23
+{
+    class Program
+    {
+        static void Sec()
+        {
+            while (true)
+            {
+                Console.WriteLine(new string(' ', 10) + "Second");
+                Thread.Sleep(200);
+            }
+        }
+
+        static void Main()
+        {
+            ThreadStart sec = new ThreadStart(Sec);
+            Thread thread = new Thread(sec);
+            thread.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Primary");
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ LB6/2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace LB6PROPolynkoKN23
+{
+    class Program
+    {
+        static bool isRunning = true; // Прапорець для завершення потоку
+
+        static void Sec()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(800); // 800 мс, бо дві "Primary" по 400 мс = 800 мс
+                Console.Write(" Second");
+            }
+        }
+
+        static void Main()
+        {
+            ThreadStart sт = new ThreadStart(Sec); // Ім'я делегата - sт
+            Thread SТ = new Thread(sт); // Ім'я потоку - SТ
+            SТ.Start();
+
+            int primaryCount = 0;
+
+            while (isRunning)
+            {
+                Console.Write("Primary");
+                primaryCount++;
+
+                // Виводимо "Second" після кожних двох "Primary" у тому ж рядку
+                if (primaryCount % 1 == 0)
+                {
+                    Console.WriteLine(); // Перехід на новий рядок після пари
+                }
+
+                Thread.Sleep(400);
+
+                // Завершення програми при натисненні клавіші
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey();
+                    isRunning = false;
+                }
+            }
+
+            // Чекаємо завершення потоку "SТ"
+            SТ.Join();
+            Console.WriteLine("\nЗавершення програми...");
+        }
+    }
+}
